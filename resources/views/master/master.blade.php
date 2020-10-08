83a484e89c892c75309a63e719b7a6008db2d35f
Nobeul <nobeul.cse@gmail.com>
2020-10-09 00:41:25 +0600

    cloned from git

---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Home | Smart Traffic</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body>
+
+    <div class="hold-transition layout-top-nav">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  @include('master.nav')
+  <!-- /.navbar -->
+
+  <!-- Content Wrapper. Contains page content -->
+    @yield('content')
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+
+  @include('master.footer')
+<!-- ./wrapper -->
+</div>
+</div>
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+@yield('js')
+</body>
+</html>
---
+++ resources/views/traffic/index.blade.php
@@ -0,0 +1,46 @@
+@extends('master.master')
+@section('content')
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-sm-6">
+            <form action="">
+                @csrf
+          <!-- textarea -->
+          <div class="form-group">
+            <label></label>
+            <textarea class="form-control" rows="2"name="uid" id="uid" placeholder="Please Tag your Card / Key Chain to display ID"></textarea>
+          </div>
+        </form>
+        </div>
+      </div>
+</div>
+
+@endsection
+
+@section('js')
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function(){
+        function uid() {
+            var token = "{{ csrf_token() }}";
+            $.ajax({
+                type: "GET",
+                url: "{{ route('getuid') }}",
+                async:false,
+                data: {
+                        "_token" : token,
+                        },
+                success:function(data){
+                    $('#uid').val(data);
+                }
+            });
+        }
+        setInterval(function () {
+            uid();
+        },500);
+    });
+</script>
+
+@endsection()
